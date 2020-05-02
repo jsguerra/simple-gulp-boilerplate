d2f089d7d9375b5c238f0e84649b1af68a98248e
Jose Guerra <jose@gcostudios.com>
2020-05-02 14:28:17 +0100

    initial setup

---
+++ gulpfile.babel.js
@@ -0,0 +1,111 @@
+// Set Gulp variables
+const { src, dest, watch, series, parallel } = require('gulp');
+
+// Set style variables
+const sass = require('gulp-sass'),
+      cssnano = require("cssnano"),
+      postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      sourcemaps = require('gulp-sourcemaps');
+
+// Set sass compiler
+sass.compiler = require('node-sass');
+
+// Set js variables
+const babel = require('gulp-babel'),
+      rename = require('gulp-rename'),
+      uglify = require('gulp-uglify');
+  
+// Set browser sync variable
+const browserSync = require('browser-sync').create();
+
+// Setup file paths
+const source = './src',
+      scss = source + '/sass/',
+      js = source + '/js/',
+      app = './app';
+
+// Styles task
+const styles = (cb) => {
+  return src(scss + '**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'expanded',
+      indentType: 'tab',
+      indentWidth: '1'
+    }).on('error', sass.logError))
+    .pipe(
+      postcss(
+        [
+          autoprefixer('last 2 versions', '> 1%'),
+          cssnano()
+        ]
+      ))
+    .pipe(sourcemaps.write())
+    .pipe(dest(app))
+    .pipe(browserSync.stream())
+
+  cb();
+}
+
+// Javascript task
+const scripts = (cb) => {
+  return src(js + '**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: [
+        [
+          '@babel/env',
+          {
+            "useBuiltIns": "usage",
+            "corejs": "3",
+            "targets": {
+              "browsers": [
+                "last 5 versions",
+                "ie >= 8"
+              ]
+            }
+          }
+        ]
+      ]
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(dest(app + '/js/'))
+
+  cb();
+}
+
+// HTML task
+const htmlTask = (cb) => {
+  return src(source + '/**/*.html')
+    .pipe(dest(app));
+
+  cb();
+}
+
+// Server task
+const startServer = (cb) => {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    }
+  })
+  cb();
+}
+
+// Watch Task
+const watchTask = (cb) => {
+  watch([scss + '**/*.scss'], styles);
+  watch([js + '**/*.js'], scripts).on('change', browserSync.reload);
+  watch([source + '/**/*.html'], htmlTask).on('change', browserSync.reload);
+
+  cb();
+}
+
+// Exports
+exports.default = series(
+  parallel(styles, scripts, htmlTask),
+  parallel(startServer, watchTask)
+);
---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+const bable = 'babel';
+
+const test = () => console.log(`this is my ${bable}`);
+
+test();
+
+console.log('another k script')
